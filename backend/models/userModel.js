75b340f125191848c55549b7a641a310d4d71a09
amanchoudhary18 <aman.choudhary9785@gmail.com>
2022-02-19 12:17:11 +0530

    first-commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default:
+        "https://www.dreamstime.com/default-avatar-profile-icon-vector-social-media-user-image-image182145777",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// userSchema.methods.matchPassword = async function (enteredPassword) {
+//   return await bcrypt.compare(enteredPassword, this.password);
+// };
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
